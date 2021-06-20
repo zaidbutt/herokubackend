77f65daa9e18f9ff81e8cce680fdd246b65aa9fc
zaid butt <xaid.butt.10@gmail.com>
2021-06-20 16:55:57 +0500

    new

--- auctions/views.py
+++ auctions/views.py
@@ -193,6 +193,8 @@ class ListingViewSet(viewsets.ModelViewSet):
     def create(self, request):
         print(request.data)
         serializer = self.get_serializer(data=request.data)
+        print(serializer)
+        print("hereeeeeeeeeeeeeee")
         print(serializer.is_valid(raise_exception=True))
         self.perform_create(serializer)
         return Response( status=status.HTTP_201_CREATED)
